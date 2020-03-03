6409f66f4f36d3bbd2af9ec2527efda44cf117c6
Edgar Alain Ramos Mesas <alain42117@hotmail.com>
2020-03-02 20:36:02 -0600

    Create exam_Server.py

---
+++ Examen/exam_Server.py
@@ -0,0 +1,35 @@
+import socket
+import marshal
+import pickle
+
+
+class matriz:
+    #def __init__(self):    
+    m=[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+    sopa=[]
+    def __init__(self):
+        for i in range(15):
+           self.sopa.append(self.m[0][0])
+        for i in range(15):
+            print(self.sopa[i])
+            
+
+
+def main():    
+    HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+    PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+   
+    dato=matriz.m
+    dato=pickle.dumps(dato);
+    for i in range(15):
+        print(matriz.m[i])
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                
+                conn.sendall(dato)
+main()
